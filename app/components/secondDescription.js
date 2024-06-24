7cb106b022a151552ad9e4f51694d6e814bfb352
biancaroh0424 <biancaroh0424@gmail.com>
2024-06-24 21:56:29 +0200

    .

---
+++ app/components/secondDescription.js
@@ -0,0 +1,11 @@
+const SecondDescription = ()=>{
+    return(
+        <div className="secondDescription">
+            <p className="smallBody">
+            Con nuovi arrivi ogni giorno, troverai sempre qualcosa di fresco e alla moda<br/> per arricchire il tuo guardaroba. Vieni a scoprire le ultime tendenze e le<br/> migliori offerte nel nostro negozio: la moda a prezzi imbattibili ti aspetta!
+            </p>
+        </div>
+    )
+}
+
+export default SecondDescription;
--- app/page.js
+++ app/page.js
@@ -1,12 +1,11 @@
 'use client'
-import styles from './globals.css';
 import Marquee from './components/marquee';
 import HeroVideoSection from './components/heroVideoSection';
 import DecorationBig from './components/decorationBig';
 import FirstDescription from './components/firstDescription';
 import StockModa from './components/stockModa'
 import FirstSlider from './components/firstSlider';
-
+import SecondDescription from './components/secondDescription';
 export default function Home() {
   return (
     <main>
@@ -16,11 +15,7 @@ export default function Home() {
     <DecorationBig/>
     <FirstDescription/>
     <FirstSlider/>
-    <div className="secondDescription">
-            <p className="smallBody">
-            Con nuovi arrivi ogni giorno, troverai sempre qualcosa di fresco e alla moda<br/> per arricchire il tuo guardaroba. Vieni a scoprire le ultime tendenze e le<br/> migliori offerte nel nostro negozio: la moda a prezzi imbattibili ti aspetta!
-            </p>
-        </div>
+    <SecondDescription/>
     </main>
   );
 }
